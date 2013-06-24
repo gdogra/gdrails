c55442d2caee9598695e0e3f88b9560f5d58f894
Dogra <gdogra@wl00031046.active.local>
2013-06-24 11:54:34 -0700

    Added all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gdrails::Application.config.secret_token = 'a8710f00f8d65c4295debdc4635a38e9b88d771044c91cca878c98e396029ce1756bfbe7b1ad9fa321fef0bacfbfd53cf966944c2008e40f298211cbb3b67fa0'
